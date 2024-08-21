e77f650c70471dc794586d5498061f5155183aa0
Leo Gimenez <leogimenez.dev@gmail.com>
2024-08-21 13:15:57 -0300

    url api

--- clima.py
+++ clima.py
@@ -5,3 +5,6 @@ import datetime
 # Clave de API de OpenWeatherMap
 api_key = 'API_KEY'
 ciudad = 'Ciudad'
+
+# URL de la API para obtener el clima actual
+url = f'http://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={api_key}&units=metric'
